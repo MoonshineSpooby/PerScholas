ff3e1dc0130c43034d25c7dbb5e4b008f16c8e9b
MoonshineSpooby <tja411@gmail.com>
2024-09-20 10:26:27 -0700

    last push for pre-work

--- LearnJavaScript/MobileApp/hosting/Assets/script.js
+++ LearnJavaScript/MobileApp/hosting/Assets/script.js
@@ -1,21 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { initializeApp } from "firebase/app";
 import { getDatabase, 
     ref, 
     push
- } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+ } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //"https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js"
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: "AIzaSyDh3hKvDAjTOj-NKDu5V9VyXdiBuoyUvjc",
-    // authDomain: "chromeext-a9782.firebaseapp.com",
-    // databaseURL: "https://chromeext-a9782-default-rtdb.firebaseio.com",
-    // projectId: "chromeext-a9782",
-    // storageBucket: "chromeext-a9782.appspot.com",
-    // messagingSenderId: "90286706086",
-    // appId: "1:90286706086:web:ff64afb191c65056a35367"
-    databaseURL: process.env.DATABASE_URL
+    apiKey: "AIzaSyDh3hKvDAjTOj-NKDu5V9VyXdiBuoyUvjc",
+    authDomain: "chromeext-a9782.firebaseapp.com",
+    databaseURL: "https://chromeext-a9782-default-rtdb.firebaseio.com",
+    projectId: "chromeext-a9782",
+    storageBucket: "chromeext-a9782.appspot.com",
+    messagingSenderId: "90286706086",
+    appId: "1:90286706086:web:ff64afb191c65056a35367"
   };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
